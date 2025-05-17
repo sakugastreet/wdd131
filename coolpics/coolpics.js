a19ea946e6f9f2483f846cdc78ef3e415fb31cdb
Joseph Earl <78830164+sakugastreet@users.noreply.github.com>
2025-05-17 13:48:34 -0600

    week 4 completed

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,6 +1,7 @@
 const menu = document.body.querySelector("button")
 const links = document.body.querySelectorAll("a")
 let hidden = true
+const gallery = document.body.querySelector("section.gallery")
 
 menu.addEventListener("click", function() {
     if (hidden) {
@@ -13,4 +14,20 @@ menu.addEventListener("click", function() {
             link.classList.add("hidden"))
             hidden = true
     }
+})
+
+let modal = document.createElement("dialog");
+modal.innerHTML = "<img></img><button id='closeBtn'>close</button>";
+document.body.appendChild(modal);
+
+modal.querySelector("#closeBtn").addEventListener("click", () => {
+    modal.close();
+});
+
+gallery.addEventListener("click", function(event) {
+    let imgpath = event.target.closest("img").src;
+
+    modal.querySelector("img").src = imgpath.split("-")[0]+"-full.jpeg";
+    modal.showModal()
+    
 })
