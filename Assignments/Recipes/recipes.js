c0de2620493ce6bbcdf592f45d685418b2688add
Joseph Earl <78830164+sakugastreet@users.noreply.github.com>
2025-06-28 12:40:43 -0600

    recipes

--- Assignments/Recipes/recipes.js
+++ Assignments/Recipes/recipes.js
@@ -279,18 +279,115 @@ const recipes = [
 		rating: 4
 	}]
 
-const gallery = document.body.querySelector(".gallery")
-for (let i = 0; i < recipes.length; i++) {
-	recipe = recipes[i]
-  	let recipeDiv = document.createElement("div");
-	recipeDiv.className = "recipe";
-	recipeDiv.innerHTML = `
-	<img src='${recipe.image}' alt="recipe img">
-	<div class="recipe-info">
-    <p class="recipe_name">${recipe.name}</p>
-    <p class="description">${recipe.description}</p>
-    <p class="rating">${recipe.rating}</p>
-	</div>`;
 
-	gallery.appendChild(recipeDiv);
+function getRandomRecipe() {
+	let randomInt = Math.floor(Math.random() * recipes.length)
+	return recipes[randomInt]
 }
+
+
+// for (let i = 0; i < recipes.length; i++) {
+// 	recipe = recipes[i]
+//   	let recipeDiv = document.createElement("div");
+// 	recipeDiv.className = "recipe";
+// 	recipeDiv.innerHTML = `
+// 	<img src='${recipe.image}' alt="recipe img">
+// 	<div class="recipe-info">
+//     <p class="recipe_name">${recipe.name}</p>
+//     <p class="description">${recipe.description}</p>
+//     <p class="rating">${recipe.rating}</p>
+// 	</div>`;
+
+// 	gallery.appendChild(recipeDiv);
+// }
+
+function recipeTemplate(recipe) {
+	return `<figure class="recipe">
+	<img src="${recipe.image}" alt="image of ${recipe.name}" />
+	<figcaption>
+		<ul class="recipe__tags">
+			${tagsTemplate(recipe.tags)}
+		</ul>
+		<h2><a href="#">${recipe.name}</a></h2>
+		<p class="recipe__ratings">
+			${ratingTemplate(recipe.rating)}
+		</p>
+		<p class="recipe__description">
+			${recipe.description}
+		</p>
+</figcaption>
+</figure>`;
+}
+
+function tagsTemplate(tags) {
+	// loop through the tags list and transform the strings to HTML
+	html = ""
+	tags.forEach(tag => {
+		html += `<li>${tag}</li>`
+	});
+	return html;
+}
+
+function ratingTemplate(rating) {
+	// begin building an html string using the ratings HTML written earlier as a model.
+	let html = `<span
+	class="rating"
+	role="img"
+	aria-label="Rating: ${rating} out of 5 stars"
+	>`
+// our ratings are always out of 5, so create a for loop from 1 to 5
+
+		// check to see if the current index of the loop is less than our rating
+		// if so then output a filled star
+
+		// else output an empty star
+	
+	for (let i=0; i<5; i++) {
+		if (i < rating){
+			html += `<span aria-hidden="true" class="icon-star">⭐</span>`
+		}
+		else {
+			html += `<span aria-hidden="true" class="icon-star-empty">☆</span>`
+		}
+		
+				
+	}
+	// after the loop, add the closing tag to our string
+	html += `</span>`
+	// return the html string
+	return html
+}
+
+function renderRecipes(RecipeList) {
+	let gallery = document.body.querySelector(".gallery")
+	let html = ""
+	RecipeList.forEach(recipe => {
+		html += recipeTemplate(recipe)
+	});
+	gallery.innerHTML = html
+}
+
+function init() {
+	renderRecipes([getRandomRecipe()])
+}
+
+function filterRecipes(text) {
+  return recipes.filter(recipe => searchList(recipe, text));
+}
+
+function searchList(recipe, text) {
+  return recipe.name.toLowerCase().includes(text);
+}
+
+init();
+
+const search = document.body.querySelector(".search");
+
+search.addEventListener("click", function () {
+  let searchText = document.body.querySelector(".search_text").value.toLowerCase();
+  if (searchText !== "") {
+    renderRecipes(filterRecipes(searchText));
+  }
+});
+
+
