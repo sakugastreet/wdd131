c4ff452398de13a272694a9911b9c2d7fbc8225d
Joseph Earl <78830164+sakugastreet@users.noreply.github.com>
2025-07-19 11:30:03 -0600

    snortblat the G

---
+++ Assignments/character-card/script.js
@@ -0,0 +1,38 @@
+const character = {
+      name: "Snortleblat",
+      class: "Swamp Beast Diplomat",
+      level: 5,
+      health: 100,
+      image: 'https://andejuli.github.io/wdd131/character_card/snortleblat.webp',
+      attacked() {
+        if (this.health >= 20) {
+          this.health -= 20;
+        } else {
+            alert('Character Died');
+        }
+      },
+      levelUp() {
+        this.level += 1;
+        this.health += 20;
+      }
+    };
+
+document.body.querySelector("img").src = character.image
+document.body.querySelector(".name").innerHTML = `<p>${character.name}</p>`
+document.getElementById("class").textContent = character.class;
+let level = document.getElementById("level")
+level.textContent = character.level;
+let health = document.getElementById("health")
+health.textContent = `${character.health}`;
+
+let levelUpBut = document.getElementById("levelup")
+let attackedBut = document.getElementById("attacked")
+
+levelUpBut.addEventListener("click", () => {
+  character.levelUp();
+  level.textContent = character.level; // optional: update the DOM
+});
+
+attackedBut.addEventListener("click", () => {
+  character.attacked();
+  health.textContent = `${character.health}`;})
